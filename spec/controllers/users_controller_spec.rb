cff6043cf6143b56c7ed52a1dcd60062611c4b0b
Patrick Jones <pmichaeljones@gmail.com>
2014-10-25 23:19:11 -0600

    removing github username duplication

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,30 +3,44 @@ require 'spec_helper'
 describe UsersController do
 
   describe "POST create" do
-    it 'doesnt create a user if net/http returns a 404' do
-      post :create, name: "Patrick", github_username: "adfasdfdfaasdf"
-      expect(User.all.count).to eq(0)
+
+    context 'within invalid inputs' do
+
+      it 'doesnt create a user if net/http returns a 404' do
+        post :create, name: "Patrick", github_username: "adfasdfdfaasdf"
+        expect(User.all.count).to eq(0)
+      end
+
+      it 'sets flash on unsuccessful save' do
+
+      end
+
     end
 
-    it 'create a user if a github user exits' do
-      post :create, name: "Patrick", github_username: "pmichaeljones"
-      expect(User.all.count).to eq(1)
-    end
+    context 'with valid inputs' do
+
+      it 'create a user if a github user exits' do
+        post :create, user: { name: "Patrick", github_username: "pmichaeljones" }
+        expect(User.all.count).to eq(1)
+      end
 
 
-    it 'sets the @users variable'
+      it 'sets the @users variable' do
 
-    it 'sets flash on successful save'
+      end
 
-    it 'sets flash on unsuccessful save'
 
-    it 'sets username doesnt exist flash' do
-      post :create, name: "Patrick", github_username: "pmichaeljonesxyz", github_username_confirmation: "pmichaeljonesxyz"
-      expect(Flash[:alert]).to eq("Username doesn't exist")
+      it 'sets flash on successful save' do
+
+      end
+
+       it 'sets username doesnt exist flash' do
+        post :create, name: "Patrick", github_username: "pmichaeljonesxyz", github_username_confirmation: "pmichaeljonesxyz"
+        expect(Flash[:alert]).to eq("Username doesn't exist")
+      end
+
     end
 
   end
-
-
 
 end
