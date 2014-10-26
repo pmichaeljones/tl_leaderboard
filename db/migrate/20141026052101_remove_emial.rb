943d42adde4351671533f6a722cdfaaf4aa9afac
Patrick Jones <pmichaeljones@gmail.com>
2014-10-25 23:30:23 -0600

    added email field, added authentications

---
+++ db/migrate/20141026052101_remove_emial.rb
@@ -0,0 +1,5 @@
+class RemoveEmial < ActiveRecord::Migration
+  def change
+    add_column :users, :email, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141025003123) do
+ActiveRecord::Schema.define(version: 20141026052101) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20141025003123) do
     t.string   "streak"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "email"
   end
 
 end
