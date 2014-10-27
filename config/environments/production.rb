d434910c00d6b821660524a1faa87c62d20553c2
Patrick Jones <pmichaeljones@gmail.com>
2014-10-27 00:06:06 -0600

    added mailgun production settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,16 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :port           => ENV['MAILGUN_SMTP_PORT'],
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain         => 'tealeaf-leaderboard.herokuapp.com', #eg: 'yourappname.herokuapp.com'
+    :authentication => :plain,
+  }
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
